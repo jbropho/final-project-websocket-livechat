b37490b4b2f92594972ad239e773412af09d0cff
Irbe Krumina <irbekrm@gmail.com>
2018-05-30 10:37:42 +0100

    test setup for node

---
+++ spec/helpers/enzyme.js
@@ -0,0 +1,10 @@
+import jasmineEnzyme from 'jasmine-enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+beforeEach(function() {
+  jasmineEnzyme();
+});
+
