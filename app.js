ee280ae68c40bd6cdc8f3cbb35ab0af38e1b817c
North <blarvinius@gmail.com>
2018-05-30 14:54:22 +0100

    add routes / and /chatroom, and a form

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+
+
 const express = require('express'),
   app = express();
+
+app.use(express.static('dist'));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+app.get('/chatroom', function(req, res){
+  res.send('chat.html');
+});
 
 module.exports = app;
