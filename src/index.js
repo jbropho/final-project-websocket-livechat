807a729a9aff7aeb8a9db4eab724573caaa0e864
jordanbrooks <jorbrooks92@gmail.com>
2018-05-31 11:29:27 +0100

    install pug and set entry point for react app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import Chatroom from './Chatroom.js';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function startApp(name) {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+window.startApp = startApp;
