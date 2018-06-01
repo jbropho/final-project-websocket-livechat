9117725b693c25ab2ecf37428ab4410717cadc55
jordanbrooks <jorbrooks92@gmail.com>
2018-06-01 15:03:46 +0100

    add RoomList component with tests

---
+++ spec/RoomListSpec.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { mount } from "enzyme";
+import RoomList from "../src/RoomList";
+
+describe('RoomList', function() {
+  var makeroomList,
+      roomList,
+      props;
+  beforeEach(done => {
+    props = {};
+    makeroomList = _ => {
+      roomList = mount(<RoomList {...props}/>);
+      return roomList;
+    }
+    done();
+  });
+  it('has a div', done => {
+    const result = makeroomList().find('div');
+    expect(result.length).toBeGreaterThan(0);
+    done();
+  });
+});
