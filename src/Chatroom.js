017f6d5dc2efe1b627f61b476a19352a896ab1d3
Irbe Krumina <irbekrm@gmail.com>
2018-06-01 14:13:07 +0100

    socket related functionality separated from component

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -3,17 +3,22 @@ import Header from './chatroomHeader';
 import Message from './Message';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
-import { socket} from './client.js';
+import { listenForMessages } from './client.js';
 
 class Chatroom extends Component {
   constructor(props) {
     super(props);
     this.state = { messages: [] }
+    this.messageAdder = this.messageAdder.bind(this);   
   }
 
   componentDidMount() {
-    socket.on('message', msg => this.setState(prevState =>
-     ({ messages: prevState.messages.concat(msg) }) ));
+    listenForMessages(this.messageAdder);
+  }
+
+  messageAdder(msg){
+    this.setState(prevState =>(
+    { messages: prevState.messages.concat(msg) }) );
   }
 
   render() {
