42cb6896727b13e5729eb8a43aaebc62e073260f
jordanbrooks <jorbrooks92@gmail.com>
2018-06-01 16:22:23 +0100

    remove console.log

--- src/MessageBoard.js
+++ src/MessageBoard.js
@@ -3,7 +3,6 @@ import Message from './Message'
 
 class MessageBoard extends Component{
   render(){
-    console.log('im in the message board');
     return( 
       <div className="message-board">
          {this.props.messageList.map(msg => <Message { ...msg } />) } 
