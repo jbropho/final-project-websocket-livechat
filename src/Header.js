ae42dbc21d7f60bbf0e0346201dcc04f1f2ca7f0
Irbe Krumina <irbekrm@gmail.com>
2018-06-01 22:54:26 +0100

    leave room button and functionality

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { killSocket } from './client.js';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.leaveRoom = this.leaveRoom.bind(this);
+  }
+
+  leaveRoom(event) {
+    killSocket(this.props.name);
+    window.location.href = 'http://localhost:8080/';
+  }
+
   render() {
     return(
       <div id="chatroom-header">
-        <p className="chatroom-title"> {this.props.title} </p>
-        <p className="chatroom-welcome">Welcome {this.props.name}!</p>
+        <p className="chatroom-title"> { this.props.title } </p>
+        <p className="chatroom-welcome">Welcome { this.props.name }!</p>
+        <input type="submit" value="leave room"
+        onClick={ this.leaveRoom }></input>
       </div>
     );
   }
