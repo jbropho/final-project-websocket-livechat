fa1e7df59e9b99aaec0fb3183a2ba8c0acaad47e
Irbe Krumina <irbekrm@gmail.com>
2018-06-01 13:04:14 +0100

    the redundant sendMessage removed, some linting errors fixed

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -8,30 +8,24 @@ import { socket} from './client.js';
 class Chatroom extends Component {
   constructor(props) {
     super(props);
-    this.state = {messages: []}
+    this.state = { messages: [] }
   }
 
   componentDidMount() {
-    socket.on('message', msg => this.setState((prevState) =>{
-     return ({ messages: prevState.messages.concat(msg)})}));
+    socket.on('message', msg => this.setState(prevState =>
+     ({ messages: prevState.messages.concat(msg) }) ));
   }
 
   render() {
-    const messages = this.state.messages.map((message) => {
-      return (
-        <Message author={message.author} content={message.content} />
-      );
-    });
     return(
       <div id="chatroom-container">
-        <Header/>
-        <Sidebar/>
-        {this.state.messages.map(msg => <Message {...msg} />)}
-        <Footer name={this.props.name} />
+        <Header />
+        <Sidebar />
+        { this.state.messages.map(msg => <Message { ...msg } />) }
+        <Footer name={ this.props.name } />
       </div>
     );
   }
 }
 
-window.sendMessage = msg => socket.emit('message', msg);
 export default Chatroom;
