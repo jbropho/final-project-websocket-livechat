a9336055c95245f67d7cb0e5b78eb98469708f13
North <blarvinius@gmail.com>
2018-06-02 15:30:49 +0100

    styling text field, enter key sends

--- src/Footer.js
+++ src/Footer.js
@@ -5,20 +5,26 @@ const messageReader = _ => document.getElementById('text-field').value;
 
 const clearMessageField = _ => document.getElementById('text-field').value = '';
 
+
+const enterClick = document.addEventListener("keyup", function(event){
+  if (event.keyCode === 13) {
+    document.getElementById("send-msg").click();
+  }
+});
+
 const handlePostClick = name => {
   sendMessage({ author: name, content: messageReader() });
   clearMessageField();
-}
+};
 
 class Footer extends Component{
   render(){
     return(
       <div className="footer">
         <div className="input">
-          <input type="text" id="text-field" maxLength="142" size="142" placeholder="Enter message here..."></input>
-        </div>
-        <div>
-          <button onClick={ _ => handlePostClick(this.props.name) }> post </button>
+          <input id="text-field" maxLength="142" size="142" placeholder="Enter message here...">
+          </input>
+          <button id="send-msg" onClick={ _ => handlePostClick(this.props.name) }> &#x21e8; </button>
         </div>
       </div>
     );
