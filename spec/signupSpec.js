f4673f140973ed54d1a28d73e6a1a402758e4cb5
Irbe Krumina <irbekrm@gmail.com>
2018-06-02 15:12:25 +0000

    user signup tests refactored

--- spec/signupSpec.js
+++ spec/signupSpec.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose'),
   User = require('../models/user'),
   server = require('../server'),
   HOST = 'http://localhost',
-  PORT = '8080';
+  PORT = '8080',
+  path = `${HOST}:${PORT}/auth/signup`,
+  user = { username: 'someUser', password: 'Pass123' };
 
 beforeEach(done => {
   User.remove({}, err => {
@@ -14,17 +16,16 @@ beforeEach(done => {
 
 describe('get signup', function () {
   it('should return 200 response code', function (done) {
-    request.get(`${HOST}:${PORT}/auth/signup`, (err, res) => {
+    request.get(path, (err, res) => {
       expect(res.statusCode).toEqual(200);
       done();
     });
   });
 });
 
-describe('signing up with username and password', function() {
+describe('signing up with valid username and password', function() {
   it('should have 201 status code', function(done) {
-    const user = { form: { username: 'irbe', password: 'Pass123' } };
-    request.post(`${HOST}:${PORT}/auth/signup`, user, (err, res) => {
+    request.post(path, { form: user }, (err, res) => {
       if(err) console.log('ERROR ', err);
       expect(res.statusCode).toEqual(201);
       done();
@@ -32,8 +33,7 @@ describe('signing up with username and password', function() {
   });
 
   it('should have auth value set to true', function(done) {
-    const user = { form: { username: 'inge', password: 'Pass123' } };
-    request.post(`${HOST}:${PORT}/auth/signup`, user, (err, res, body) => {
+    request.post(path, { form: user }, (err, res, body) => {
       body = JSON.parse(body);
       if(err) console.log(err);
       expect(body["auth"]).toBe(true);
@@ -42,22 +42,19 @@ describe('signing up with username and password', function() {
   });
 
   it('should have the name of the user', function(done) {
-    const user = { form: { username: 'mirte', password: 'Pass123' } };
-    request.post(`${HOST}:${PORT}/auth/signup`, user, (err, res, body) => {
+    request.post(path, { form: user }, (err, res, body) => {
     body = JSON.parse(body);
       if(err) console.log(err);
-      expect(body["name"]).toEqual('mirte');
+      expect(body["name"]).toEqual(user.username);
       done();
     });
   });
 });
 
-describe('signing up with repeating username', function() {
+describe('attempting to signup with a taken username', function() {
   it('should have 500 status code and false auth', function(done) {
-    const user = { username: 'ilma', password: 'Pass123' };
-    const form = { form: user };
     User.create(user);
-    request.post(`${HOST}:${PORT}/auth/signup`, form, (err, res, body) => {
+    request.post(path, { form: user }, (err, res, body) => {
       if(err) console.log(err);
       expect(res.statusCode).toEqual(500); 
       done();
