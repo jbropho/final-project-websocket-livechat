14e29949ac6864d515ed3be9f028219498194258
Irbe Krumina <irbekrm@gmail.com>
2018-06-03 16:17:28 +0100

    signup logics frontend implemented

---
+++ dist/signup.js
@@ -0,0 +1,46 @@
+const HOME = 'http://localhost:8080/';
+
+window.onload = _ => listen();
+
+const listen = _ => {
+  let signup = document.getElementById('signup');
+  signup && signup.addEventListener('click', onSignup);
+}
+
+const submitUserDetails = ext => {
+  const url = `${HOME}${ext}`;
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const params = `username=${username}&password=${password}`;
+  const headers = { 'Content-type': 'application/x-www-form-urlencoded' }
+  return sendRequest('POST', url, params, headers);
+}; 
+
+async function onSignup(e) {
+  await submitUserDetails('auth/signup').then(result => processResult(result)).catch(err => {console.log(err); e.preventDefault()})
+  redirect(HOME);
+  e.preventDefault();
+};
+
+const sendRequest = (method, url, params='', headers={}) => {
+  return new Promise((res, rej) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = _ => {
+      if(xhr.readyState == 4) {
+        ~~(xhr.status/100) == 2 ? res(xhr.responseText) : rej(xhr.statusText);
+      }
+    };
+    for(let head in headers) { xhr.setRequestHeader(head, headers[head]) };
+    xhr.send(params);
+  });
+};
+
+const setStorage = result => {for(let key in result) { window.sessionStorage.setItem(key, result[key]); }};
+const processResult = result => {
+  result = JSON.parse(result);
+  setStorage(result);
+}
+
+const redirect = address => window.location.href = address;
+
