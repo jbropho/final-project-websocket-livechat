57587a0da2717017074871ce4a6a3bd1363a40d6
Jordan Brooks <jorbrooks92@gmail.com>
2018-06-03 14:24:36 +0100

    add joinRoom functionality

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const app = require('./app'),
   http = require('http').Server(app),
   port = process.env.PORT || 8080,
   io = require('socket.io').listen(http);
-  
+
 io.sockets.on('connection', socket => {
   socket.on('message', msg => {
     io.emit('message', msg);
   });
+
+  socket.on('joinRoom', roomName => {
+    socket.join(roomName);
+
+    socket.on(roomName, message => {
+      io.to(roomName).emit('message', message);
+    }) 
+  });
+
   socket.on('wantToDie', name => {
     console.log(`${name} left`);
     socket.disconnect();
   });
+
   socket.on('clientJoin', name => {
     console.log(`${name} joined`);
   });
 });
+
 http.listen(port, console.log(`Listening on port ${port}`));
 module.exports = http;
