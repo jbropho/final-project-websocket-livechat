6b1835f84a83886e52459cb4b61cd5724f2ceb0a
Matthew <matt_burgoyne@live.co.uk>
2018-06-04 16:21:21 +0100

    enter click, messageReader and clearMessageField functions to helpers file, importing all done on one line in footer.js

--- src/ChatroomComponents/Footer.js
+++ src/ChatroomComponents/Footer.js
@@ -1,21 +1,9 @@
 import React, { Component } from "react";
 import { handlePostClick } from '../helpers.js';
+import { enterClick, messageReader, clearMesssageField } from '../helpers.js';
 import { sendMessage } from '../client.js';
 
-const messageReader = _ => document.getElementById('text-field').value;
-const clearMessageField = _ => document.getElementById('text-field').value = '';
 
-const enterClick = document.addEventListener("keyup", function(event){
-  var clickedButton = document.getElementById("send-msg");
-  var userText = document.getElementById("text-field");
-  if (userText.value && event.keyCode === 13) {
-    clickedButton.click();
-    clickedButton.classList.add("active");
-    setTimeout(function() {
-      clickedButton.classList.remove("active");
-    }, 200);
-  }
-});
 
 
 
--- src/helpers.js
+++ src/helpers.js
@@ -8,18 +8,38 @@ function findWebsocketAddress() {
     'ws://finalprojectwebsocketlivechat.herokuapp.com/'
     : 'http://localhost:8080/'
 }
+const messageReader = _ => document.getElementById('text-field').value;
+const clearMessageField = _ => document.getElementById('text-field').value = '';
 
 function handlePostClick(name) {
   var string = document.getElementById('text-field').value;
   if(string) {
-  sendMessage({ author: name, content: messageReader() });
-  clearMessageField();
+    sendMessage({ author: name, content: messageReader() });
+    clearMessageField();
   }
   document.getElementById("text-field").focus();
+}
+
+function enterClick() {
+  document.addEventListener("keyup", function(event){
+    var clickedButton = document.getElementById("send-msg");
+    var userText = document.getElementById('text-field');
+    if (userText.value && event.keyCode === 13) {
+      clickedButton.click();
+      clickedButton.classList.add("active");
+      setTimeout(function() {
+        clickedButton.classList.remove("active");
+      }, 200);
+    }
+  });
+
 }
 
 export {
   updateScroll,
   handlePostClick,
+  enterClick,
+  messageReader,
+  clearMessageField,
   findWebsocketAddress
 };
