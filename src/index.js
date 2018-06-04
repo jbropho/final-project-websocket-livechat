726c17c6349e39362b5f009806f39da9d7c38b4b
Matthew <matt_burgoyne@live.co.uk>
2018-06-04 19:37:13 +0100

    north favicon rendering on the chatroom

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chatroom from './ChatroomComponents/Chatroom.js';
 
+
 function startApp(name) {
   ReactDOM.render(
     <Chatroom name={name}/>,
     document.getElementById('root')
+
   );
 }
 
