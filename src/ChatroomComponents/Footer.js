1598fbf7093252f4c75cb80673d5861e71ee0ce4
Matthew <matt_burgoyne@live.co.uk>
2018-06-04 19:12:20 +0100

    messages will submit on user pressing enter key, removed background from messages

--- src/ChatroomComponents/Footer.js
+++ src/ChatroomComponents/Footer.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import { handlePostClick, enterClick, messageReader, clearMesssageField } from '../helpers.js';
-import { handlePostClick } from '../helpers.js';
-import { enterClick, messageReader, clearMesssageField } from '../helpers.js';
 import { sendMessage } from '../client.js';
 
 class Footer extends Component{
+
   render(){
     return(
       <div className="footer">
         <div className="input">
-          <input autoFocus="true" id="text-field" maxLength="142" placeholder="Enter message here...">
+          <input autoFocus="true" id="text-field" maxLength="142" placeholder="Enter message here..." onKeyPress={enterClick(this.props.name)}>
           </input>
           <button className="btn" id="send-msg" onClick={ _ => handlePostClick(this.props.name) }> &#x21e8; </button>
         </div>
