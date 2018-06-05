e2f1c5d1374d0b97bc0f7d66647ca2e4550f84fc
Irbe Krumina <irbekrm@gmail.com>
2018-06-05 21:52:26 +0100

    merge conflict fixed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chatroom from './ChatroomComponents/Chatroom.js';
 
-<<<<<<< HEAD
 
-function startApp(name) {
-=======
 function startApp() {
->>>>>>> c587c94d28b05d3ed8232ccd52f04465b9b646cc
   ReactDOM.render(
     <Chatroom name={window.sessionStorage.name}/>,
     document.getElementById('root')
