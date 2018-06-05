0f9f5c0ef092b5e13f22f268e0295aed622c57bd
Jordan Brooks <jorbrooks92@gmail.com>
2018-06-05 12:05:21 +0100

    fix aysnch error in echoSpec

--- spec/echoSpec.js
+++ spec/echoSpec.js
@@ -14,9 +14,9 @@ describe('socket connection', function() {
       client.on('message', message => {
         expect(message).toEqual('Hello World');
         client.disconnect();
-        done();
       });
       client.emit('message', 'Hello World');
+      done();
     });
   });
 });
