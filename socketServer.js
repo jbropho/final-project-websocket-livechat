46015b8364747ea81d2ae611eea5eee8a6dd6ae4
Irbe Krumina <irbekrm@gmail.com>
2018-06-06 10:21:59 +0100

    inconsistency with message name fixed

--- socketServer.js
+++ socketServer.js
@@ -2,15 +2,12 @@ const http = require('./httpServer').http,
   io = require('socket.io').listen(http);
 
   io.sockets.on('connection', socket => {
-    socket.on('message', msg => {
-      io.emit('message', msg);
-    });
 
     socket.on('joinRoom', roomName => {
       socket.join(roomName);
     
       socket.on(roomName, msg => {
-        io.to(roomName).emit('message', msg);
+        io.to(roomName).emit(roomName, msg);
       });
     });
 
