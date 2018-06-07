6cc2247061aab1e11e0e4f4546071046ad49ac94
Jordan Brooks <jorbrooks92@gmail.com>
2018-06-07 12:14:06 +0100

    remove console.log

--- src/ChatroomComponents/RoomList.js
+++ src/ChatroomComponents/RoomList.js
@@ -1,17 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class RoomList extends Component{
 
-  render(){
-
+  render(){ 
     return(
       <div className="chatroom-list">
-        <ul>
-
-         { this.props.roomlist.map(room =>{console.log(this.props.currentRoom)
-           return <li className={room === this.props.currentRoom ? room : null} onClick= { _ => this.props.joinRoom(room) } > {room} </li>}
-         )}
-
+        <ul> d
+          { this.props.roomlist.map(room =>{
+            return <li className={room === this.props.currentRoom ? room : null} onClick= { _ => this.props.joinRoom(room) } > {room} </li>}
+          )}
         </ul>
       </div>
     );
