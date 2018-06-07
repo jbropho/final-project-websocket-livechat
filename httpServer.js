c0a912ccf55fe9647c33a1aa554b78439d1bb9e6
Jordan Brooks <jorbrooks92@gmail.com>
2018-06-07 11:47:38 +0100

    refactor singleline methods into multiline

--- httpServer.js
+++ httpServer.js
@@ -6,6 +6,16 @@ var newServer;
 
 module.exports = {
   http: http,
-  start: _ => {if (!newServer) { newServer = http.listen(port, console.log(`Listening on port ${port}`))}; return newServer},
-  stop: _ => { if(newServer) newServer.close(); console.log('CLOSING') }
-}
+  
+  start: _ => {
+    if (!newServer) {
+      newServer = http.listen(port, console.log(`Listening on port ${port}`))
+    } 
+    return newServer
+  },
+
+  stop: _ => { 
+    if(newServer) newServer.close();
+    console.log('CLOSING'); 
+  }
+};
