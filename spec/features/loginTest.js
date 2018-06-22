b6e89fb27af65a596096e941cd047c129b237aff
mattb20 <matt_burgoyne@live.co.uk>
2018-06-22 07:01:03 +0100

    first feature test for a username field written and passed

--- spec/features/loginTest.js
+++ spec/features/loginTest.js
@@ -1,3 +1,8 @@
+this.loginPageTest = function(browser) {
+  browser
+    .url('http://localhost:8080/')
+    .assert.elementPresent('#username-input');
+};
 this.loginTest = function(browser) {
   browser
     .url('http://localhost:8080/');
