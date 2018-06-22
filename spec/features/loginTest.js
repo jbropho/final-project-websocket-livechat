29bfc21cf6b7cf90f7db654eec3a0779584cafd6
mattb20 <matt_burgoyne@live.co.uk>
2018-06-22 07:05:03 +0100

    second test for password field written and passed, included the end method to close browser after the last test

--- spec/features/loginTest.js
+++ spec/features/loginTest.js
@@ -1,9 +1,11 @@
-this.loginPageTest = function(browser) {
+this.loginPageTestUsername = function(browser) {
   browser
     .url('http://localhost:8080/')
     .assert.elementPresent('#username-input');
 };
-this.loginTest = function(browser) {
+this.loginPageTestPassword = function(browser) {
   browser
-    .url('http://localhost:8080/');
+    .url('http://localhost:8080/')
+    .assert.elementPresent('#password-input')
+    .end(__ => console.log('feature tests ran using nightwatch'));
 };
