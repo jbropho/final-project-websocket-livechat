fe980b82dc8546d06a015fac23f39df5b708fb0e
mattb20 <matt_burgoyne@live.co.uk>
2018-06-23 07:40:05 +0100

    socketserver and client linted

--- socketServer.js
+++ socketServer.js
@@ -1,6 +1,6 @@
 const http = require('./httpServer').http,
   io = require('socket.io').listen(http),
-  Message = require('./models/message')
+  Message = require('./models/message');
 
 io.sockets.on('connection', socket => {
 
@@ -13,7 +13,7 @@ io.sockets.on('connection', socket => {
       content: message.content
     }, err => console.log(err));
 
-      Message
+    Message
       .find({roomName: roomName}, 'username content')
       .sort({created_at: -1})
       .limit(20)
@@ -21,27 +21,27 @@ io.sockets.on('connection', socket => {
         var prevMessages = result.map(function(message) {
           return { content: message.content,
             author: message.username
-                }
-        })
+          };
+        });
         socket.emit(roomName, prevMessages.reverse());
       });
 
-      socket.on(roomName, message => {
-        io.to(roomName).emit(roomName, message);
-        Message.create( {
-          username: message.author,
-          roomName: roomName,
-          content: message.content
-        }, err => console.log(err));
-      });
-    });
-
-    socket.on('wantToDie', name => {
-    console.log(`${name} left`);
-  socket.disconnect();
-    });
-
-    socket.on('clientJoin', name => {
-      console.log(`${name} joined`);
+    socket.on(roomName, message => {
+      io.to(roomName).emit(roomName, message);
+      Message.create( {
+        username: message.author,
+        roomName: roomName,
+        content: message.content
+      }, err => console.log(err));
     });
   });
+
+  socket.on('wantToDie', name => {
+    console.log(`${name} left`);
+    socket.disconnect();
+  });
+
+  socket.on('clientJoin', name => {
+    console.log(`${name} joined`);
+  });
+});
